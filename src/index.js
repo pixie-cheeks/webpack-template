125f219cf4919b3a5803103a9fe4071ec50ffc03
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-05-10 10:08:25 +0530

    Improve template slightly

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './style.css';
 
 console.log('Check whether everything is working or not.');
 
-if (process.env.NODE_ENV === 'development') {
-  if (module.hot) {
-    module.hot.accept();
-  }
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept();
 }
